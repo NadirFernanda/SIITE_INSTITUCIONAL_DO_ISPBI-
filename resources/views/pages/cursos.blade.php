1cc756f0e8a6a5711d3b74a5be43d3efc1907f61
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-26 23:05:55 +0100

    Tornar subtítulo 'Algumas áreas de actuação' branco nos cards (melhor contraste)

--- resources/views/pages/cursos.blade.php
+++ resources/views/pages/cursos.blade.php
@@ -170,7 +170,7 @@ $cursos = [
                         <!-- Perfis de saída / Áreas (após o título) - ordem 2 -->
                         <div class="order-2">
                             @if(count($curso['areas']) > 0)
-                            <div class="text-sm font-semibold mb-2">Algumas áreas de actuação</div>
+                            <div :class="dark ? 'text-gray-100 font-semibold text-sm mb-2' : 'text-white font-semibold text-sm mb-2'">Algumas áreas de actuação</div>
                             <div class="flex gap-4 mb-3 flex-wrap items-start">
                                 @foreach ($curso['areas'] as $area)
                                 <div :class="dark ? 'text-gray-100' : 'text-white'" class="flex flex-col items-start text-sm w-auto min-w-[140px]">
