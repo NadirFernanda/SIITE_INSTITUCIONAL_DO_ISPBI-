212955ff549412031154b2de98852e4061849105
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-27 02:20:34 +0100

    Trabalhe Connosco: update RH email to dpto.rhas@isp-bie.ao

--- resources/views/pages/trabalhe-conosco.blade.php
+++ resources/views/pages/trabalhe-conosco.blade.php
@@ -223,7 +223,7 @@
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                 </svg>
-                <span>Email: rh@ispbie.ao</span>
+                <span>Email: dpto.rhas@isp-bie.ao</span>
               </p>
               <p class="flex items-start">
                 <svg class="w-6 h-6 mr-3 flex-shrink-0 mt-1" fill="currentColor" viewBox="0 0 20 20">
