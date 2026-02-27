73c7f6c9c460be06f123f88290c204f60192be43
NadirFernanda <fernandadapiedade2018gmail.com>
2026-02-27 14:34:29 +0100

    Atualizar descrição Engenharia em Recursos Hídricos (adicionar planeamento)

--- resources/views/pages/cursos.blade.php
+++ resources/views/pages/cursos.blade.php
@@ -111,7 +111,7 @@ $cursos = [
     ],
     [
         'title' => 'Engenharia em Recursos Hídricos',
-        'description' => 'O licenciado em Engenharia em Recursos Hídricos é um profissional especializado na gestão, estudo e aproveitamento sustentável da água. Atua em projectos, fiscalização, investigação e implementação de políticas, integrando equipas multidisciplinares e avaliando impactos ambientais, contribuindo para saneamento, energia, irrigação e preservação dos recursos hídricos nos sectores público e privado.',
+        'description' => 'O licenciado em Engenharia em Recursos Hídricos é um profissional especializado na gestão, estudo e aproveitamento sustentável da água. Atua em projectos, fiscalização, investigação e implementação de políticas, integrando equipas multidisciplinares e avaliando impactos ambientais, contribuindo para saneamento, energia, irrigação, planeamento e preservação dos recursos hídricos nos sectores público e privado.',
         'department' => 'Engenharias',
         'domain' => 'Engenharias e Telecomunicações',
         'duration' => '6 Anos',
