7f0334f05083ab38c1a1d307620d9f87c94538d5
NadirFernanda <fernandadapiedade2018gmail.com>
2026-03-13 16:23:38 +0100

    Fix CSP: add unsafe-eval to allow Alpine.js expression evaluation

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -25,7 +25,7 @@ class SecureHeaders
         $response->headers->set(
             'Content-Security-Policy',
             "default-src 'self'; " .
-            "script-src 'self' 'unsafe-inline'; " .
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " .
             "style-src 'self' 'unsafe-inline'; " .
             "img-src 'self' data: https:; " .
             "font-src 'self' data:; " .
