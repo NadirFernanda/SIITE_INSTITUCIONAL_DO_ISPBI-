6f5c57b79c134952434f0b8484772c480db52591
NadirFernanda <fernandadapiedade2018gmail.com>
2026-04-03 23:07:26 +0100

    Add Portuguese url validation message

--- lang/pt/validation.php
+++ lang/pt/validation.php
@@ -3,6 +3,7 @@
 return [
     'required' => 'O campo :attribute é obrigatório.',
     'email' => 'O campo :attribute deve ser um endereço de email válido.',
+    'url' => 'O campo :attribute deve ser um URL válido.',
     'min' => [
         'string' => 'O campo :attribute deve ter pelo menos :min caracteres.',
     ],
@@ -12,5 +13,6 @@ return [
     'attributes' => [
         'email' => 'usuário',
         'password' => 'senha',
+        'link' => 'link',
     ],
 ];
